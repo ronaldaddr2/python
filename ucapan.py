a130fe5c1c6201fd5ed9773f02c25172705a1323
Ronald adriansyah <sschouse@gmail.com>
2019-09-13 05:32:59 +0700

    Add files via upload

---
+++ ucapan.py
@@ -0,0 +1,11 @@
+
+salam = input('Kata Anda : ')
+
+if salam == 'pagi':
+   print ('selamat', salam)
+elif salam == 'siang':
+   print ('Selamat', salam)
+elif salam == 'malam':
+   print ('Selamat', salam)
+else:
+   print ('Selamat Jalan')
